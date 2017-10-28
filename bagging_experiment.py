439ee74e3fa7690ed5d4a9fb1219e451afd85997
Charles EM Reynolds <reynoldscem@gmail.com>
2017-10-28 18:14:59 +0100

    Develop training code for bagging experiment

--- bagging_experiment.py
+++ bagging_experiment.py
@@ -1,6 +1,6 @@
 from lasagne.layers import (
     InputLayer, DenseLayer, get_all_params, get_all_param_values, get_output)
-from lasagne.objectives import categorical_crossentropy
+from lasagne.objectives import categorical_crossentropy, categorical_accuracy
 from lasagne.nonlinearities import softmax
 from lasagne.updates import adam
 
@@ -23,13 +23,19 @@ def build_parser():
         required=True
     )
 
+    parser.add_argument(
+        '-m', '--mean-path',
+        help='Path to npy file containing dataset mean.',
+        default='cifar_mean.npy'
+    )
+
     return parser
 
 
 def build_network(
         input_var=None,
         batch_size=None, feature_dimensionality=3072,
-        hidden_size=4000, output_classes=10):
+        hidden_size=1000, output_classes=10):
     network = OrderedDict()
 
     network['input'] = InputLayer(
@@ -40,9 +46,12 @@ def build_network(
     network['fc1'] = DenseLayer(
         network['input'], hidden_size
     )
+    network['fc2'] = DenseLayer(
+        network['fc1'], hidden_size
+    )
 
     network['output'] = DenseLayer(
-        network['fc1'],
+        network['fc2'],
         output_classes,
         nonlinearity=softmax
     )
@@ -86,7 +95,18 @@ def load_data(dataset_directory, dataset_mean):
     assert train_data.shape[1] == dataset_mean.shape[0]
     train_data = train_data - dataset_mean
 
+    train_data = train_data.astype(theano.config.floatX)
+    train_labels = train_labels.astype(np.int32)
     return train_data, train_labels
+
+
+def load_mean(mean_path):
+    abs_mean_path = os.path.abspath(mean_path)
+    assert os.path.isfile(abs_mean_path), (
+        '{} is not a file!'.format(abs_mean_path)
+    )
+
+    return np.load(mean_path)
 
 
 def train_val_split(X, y, val_proportion=0.1):
@@ -100,12 +120,63 @@ def train_val_split(X, y, val_proportion=0.1):
 
 def main():
     args = build_parser().parse_args()
-    X, y = load_data(args.dataset_directory, np.zeros(3072))
+
+    dataset_mean = load_mean(args.mean_path)
+    X, y = load_data(args.dataset_directory, dataset_mean)
 
     train_X, train_y, val_X, val_y = train_val_split(X, y)
 
     print([mat.shape for mat in (train_X, train_y, val_X, val_y)])
 
+    # import IPython
+    # IPython.embed()
+
+    input_var = T.matrix('input', dtype=theano.config.floatX)
+    target = T.vector('target', dtype='int32')
+
+    network = build_network(input_var=input_var)
+    prediction = get_output(network['output'])
+    loss = categorical_crossentropy(prediction, target).mean()
+    accuracy = categorical_accuracy(prediction, target).mean()
+
+    params = get_all_params(network['output'], trainable=True)
+    updates = adam(loss, params)
+
+    train_function = theano.function(
+        [input_var, target],
+        loss,
+        updates=updates
+    )
+    loss_function = theano.function(
+        [input_var, target],
+        loss
+    )
+    accuracy_function = theano.function(
+        [input_var, target],
+        accuracy
+    )
+
+    n_chunks = 50
+    train_chunks = list(zip(
+        np.split(train_X, n_chunks),
+        np.split(train_y, n_chunks)
+    ))
+    epoch_losses = []
+    validation_losses = []
+    for epoch in range(1, 250):
+        epoch_loss = 0.
+        for X_chunk, y_chunk in train_chunks:
+            epoch_loss += train_function(X_chunk, y_chunk) / n_chunks
+
+        epoch_losses.append(epoch_loss)
+        print('Epoch {}\n\ttrain loss: {}'.format(epoch, epoch_loss))
+
+        validation_loss = float(loss_function(val_X, val_y))
+        validation_accuracy = float(accuracy_function(val_X, val_y) * 100.)
+        print('\tval loss: {}'.format(validation_loss))
+        print('\tval accuracy: {:.2f}%'.format(validation_accuracy))
+
+        validation_losses.append(validation_loss)
     import IPython
     IPython.embed()
 
---
+++ run_exps.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Assumes your theanorc is set properly.
+python bagging_experiment.py \
+  -d ../cifar/cifar-10-batches-py
